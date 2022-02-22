3bd7d6c8cb70f5e8c0a6a8e5712260346a7cd763
Noam Cohen <noamcohe@g.jct.ac.il>
2022-02-22 19:28:31 +0200

    none

--- src/Animal.java
+++ src/Animal.java
@@ -1,13 +1,13 @@
-public abstract class Animal implements Seasonable,Comparable{
+public abstract class Animal implements Seasonable,Comparable<Animal>{
     protected int weight;
     private Season season;
     protected Color color;
     // TODO: Add auxiliary fields and functions.
 
     Animal(int weight, Season season, Color color){
-        this.weight =weight;
-        this.season =season;
-        this.color =color;
+        this.weight = weight;
+        this.season = season;
+        this.color = color;
     }
 
     @Override
@@ -16,8 +16,14 @@ public abstract class Animal implements Seasonable,Comparable{
     }
 
     @Override
-    public int compareTo(Object o) {
-        // TODO: Implement.
-        return 0;
+    public int compareTo(Animal o)
+    {
+        return Integer.compare(this.weight, o.weight);
+    }
+
+    @Override
+    public void changeSeason()
+    {
+        season = Season.getNext(this.season);
     }
 }
